7317f7047ca1e2a1cc2a45c8457a8c399270bd70
NistorElena-Simona <127746383+NistorElena-Simona@users.noreply.github.com>
2023-03-13 13:48:55 +0200

    Add files via upload

---
+++ Farmacie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Farmacie
+    {
+        string denumire;
+        int nrPastile;
+        public int TipPastile;
+        private string _denumire;
+        private int _nrPastile;
+
+        public Farmacie()
+        {
+            denumire = _denumire;
+            nrPastile = _nrPastile;
+        }
+        public Farmacie(string denumire, int nrPastile, int tipPastile)
+        {
+            _denumire = denumire;
+            _nrPastile = nrPastile;
+            TipPastile = tipPastile;
+            
+        }
+    }
+
+}
