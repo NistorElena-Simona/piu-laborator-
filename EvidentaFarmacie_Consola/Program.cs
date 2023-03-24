34f9640b5fb2d07f2f26101330ab073f6e76ae42
NistorElena-Simona <127746383+NistorElena-Simona@users.noreply.github.com>
2023-03-24 18:36:14 +0200

    Add files via upload

---
+++ EvidentaFarmacie_Consola/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Configuration;
+using LibrarieModele;
+using NivelStocareDate;
+
+namespace EvidentaFarmacie_Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Farmacie farmacie = new Farmacie("Catena");
+            
+
+
+            string numeFisier= ConfigurationManager.AppSettings ["NumeFisier"];
+            AdministrareMedicamente_FisierText adminMedicament = new AdministrareMedicamente_FisierText(numeFisier);
+            Medicament[] lista_medicamente_fisier = adminMedicament.GetmedicamentedinFisier(numeFisier);
+            foreach (Medicament med in lista_medicamente_fisier)
+            {
+                farmacie.AddMedicament(med);
+            }
+
+
+
+
+            int nr_medicamente = 0;
+            adminMedicament.CautaMedicamenteDupaNrPastile(out nr_medicamente);
+            string optiune;
+            do
+            {
+                Console.WriteLine("I. Adaugare medicament");
+                Console.WriteLine("A. Afisare medicamente");
+                Console.WriteLine("F. Afisare medicamente din fisier");
+                Console.WriteLine("S. Salvare medicament in fisier");
+                Console.WriteLine("L. Cauta medicament dupa nume");
+                Console.WriteLine("X. Inchidere program");
+                Console.WriteLine("Alegeti o optiune");
+                optiune = Console.ReadLine();
+                switch (optiune.ToUpper())
+                {
+                    case "I":
+                        int idMedicament = nr_medicamente + 1;
+
+                        Console.Write("Introdu numele medicamentului : ");
+                        string nume_medicament = Console.ReadLine();
+                        Console.Write("Introdu  cate cutii  : ");
+                        string  nr_cutii = Console.ReadLine();
+                        Medicament medicament = new Medicament(idMedicament, nume_medicament, nr_cutii);
+                        ///ADAUGARE STUDENT IN FISIER
+                        adminMedicament.AddMedicament( medicament );
+                        nr_medicamente++;
+                        idMedicament++;
+                        break;
+                    case "A":
+                        foreach (Medicament med in farmacie.listaMedicamente)
+                        {
+                            if (med == null) break;
+
+                            Console.WriteLine($"Medicamentul {med.Info()}");
+
+                        }
+
+                        break;
+                    case "F":
+                        //AfisareMedicamente( nume_medicament, nr_medicamente);
+
+                        break;
+                    case "S":
+                        int idMedicamente = nr_medicamente + 1;
+                        nr_medicamente++;
+                        Medicament medicament1 = new Medicament (idMedicamente, "Ibuprofen", "200");
+                        //adaugare student in fisier
+                        adminMedicament.AddMedicament(medicament1);
+
+                        break;
+                    case "L":
+
+                        Console.WriteLine("INTRODUCETI NUMELE Medicamentului:");
+                        string nume_cautat = Console.ReadLine();
+
+                        Medicament medicament_gasit = farmacie.GetMedicament(nume_cautat);
+                        if (medicament_gasit == null)
+                        {
+                            Console.WriteLine("Nu exista acest medicament");
+                            break;
+                        }
+                        Console.WriteLine($"Medicament gasit: {medicament_gasit.Info()}");
+                        break;
+
+                    case "X":
+
+                        return;
+                    default:
+                        Console.WriteLine("Optiune inexistenta");
+
+                        break;
+                }
+            } while (optiune.ToUpper() != "X");
+
+            Console.ReadKey();
+        }
+
+        //public static void AfisareMedicamente(Medicament[] n, int nr_medicamente)
+        //{
+            //Console.WriteLine("Studentii sunt:");
+            //for (int contor = 0; contor < nr_medicamente; contor++)
+            //{
+                //string infoStudent = string.Format("Studentul cu id-ul #{0} are numele: {1} {2}",
+                   //studenti[contor].GetIdStudent(),
+                   //studenti[contor].GetNume() ?? " NECUNOSCUT ",
+                   //studenti[contor].GetPrenume() ?? " NECUNOSCUT ");
+
+                //Console.WriteLine(infoMedicament);
+           // }
+        //}
+
+    
+     }
+}
---
+++ NivelStocareDate/AdministrareMedicamente_FisierText.cs
@@ -0,0 +1,93 @@
+﻿using LibrarieModele;
+
+using System.IO;
+
+namespace NivelStocareDate
+{
+    public class AdministrareMedicamente_FisierText
+    {
+        private const int NR_MAX_Medicamente = 50;
+        private string numeFisier;
+
+        public AdministrareMedicamente_FisierText(string numeFisier)
+        {
+            this.numeFisier = numeFisier;
+            // se incearca deschiderea fisierului in modul OpenOrCreate
+            // astfel incat sa fie creat daca nu exista
+            Stream streamFisierText = File.Open(numeFisier, FileMode.OpenOrCreate);
+            streamFisierText.Close();
+        }
+
+        public void AddMedicament(Medicament medicament)
+        {
+            // instructiunea 'using' va apela la final streamWriterFisierText.Close();
+            // al doilea parametru setat la 'true' al constructorului StreamWriter indica
+            // modul 'append' de deschidere al fisierului
+            using (StreamWriter streamWriterFisierText = new StreamWriter(numeFisier, true))
+            {
+                streamWriterFisierText.WriteLine(medicament.ConversieLaSir_PentruFisier());
+            }
+        }
+
+        public Medicament[] CautaMedicamenteDupaNrPastile(out int nr_pastile)
+        {
+            Medicament[] medicament = new Medicament[NR_MAX_Medicamente];
+
+            // instructiunea 'using' va apela streamReader.Close()
+            using (StreamReader streamReader = new StreamReader(numeFisier))
+            {
+                string linieFisier;
+                nr_pastile = 0;
+
+                // citeste cate o linie si creaza un obiect de tip Student
+                // pe baza datelor din linia citita
+                while ((linieFisier = streamReader.ReadLine()) != null)
+                {
+                    medicament[nr_pastile++] = new Medicament(linieFisier);
+                }
+
+            }return medicament;
+        }
+            
+            public Medicament[] GetmedicamentedinFisier(string numeFisier)
+        {
+            Medicament[] medicamente = new Medicament[NR_MAX_Medicamente];
+
+            // instructiunea 'using' va apela streamReader.Close()
+            using (StreamReader streamReader = new StreamReader(numeFisier))
+            {
+                string linieFisier;
+                int index_medicament = 0;
+
+
+                // citeste cate o linie si creaza un obiect de tip Student
+                // pe baza datelor din linia citita
+                while ((linieFisier = streamReader.ReadLine()) != null)
+                {
+                    medicamente[index_medicament++] = new Medicament(linieFisier);
+                }
+                return medicamente;
+            }
+
+            
+        }///implementare functie la pct 5
+        public Medicament GetMedicament(string nume_medicament, string nr_medicamente)
+        {
+            using (StreamReader streamReader = new StreamReader(numeFisier))
+            {
+                string linieFisier;
+                ////
+                ///
+                while ((linieFisier = streamReader.ReadLine()) != null)
+                {
+                    Medicament medicament = new Medicament(linieFisier);
+                    if (medicament.GetNume_medicament() == nume_medicament && medicament.GetNr_Medicamente() == nr_medicamente)
+                    {
+                        return medicament;
+                    }
+                }
+            }
+            return new Medicament();
+        }
+    }
+}
